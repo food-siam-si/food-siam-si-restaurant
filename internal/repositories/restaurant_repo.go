997e421e30e96135d5c0420a234e93086be7d5ab
Pimrak Apiratchatanont <apimrak@gmail.com>
2023-09-20 10:00:55 +0700

    feat: implement repository

--- internal/repositories/restaurant_repo.go
+++ internal/repositories/restaurant_repo.go
@@ -23,21 +23,59 @@ func (r *restaurantRepository) Create(payload *domain.Restaurant) error {
 }
 
 func (r *restaurantRepository) Update(id uint, payload *domain.Restaurant) error {
-	return nil
+	restaurant := models.ParseRestaurant(payload)
+
+	return r.db.Model(&restaurant).Where("id = ?", id).Updates(restaurant).Error
 }
 
 func (r *restaurantRepository) FindById(id uint) (domain.Restaurant, error) {
-	return domain.Restaurant{}, nil
+	restaurant := models.Restaurant{}
+
+	err := r.db.Where("id = ?", id).Preload("Types").First(&restaurant).Error
+
+	return *restaurant.ToDomain(), err
 }
 
 func (r *restaurantRepository) FindAll() ([]domain.Restaurant, error) {
-	return []domain.Restaurant{}, nil
+	restaurants := []models.Restaurant{}
+
+	err := r.db.Preload("Types").Find(&restaurants).Error
+
+	if err != nil {
+		return []domain.Restaurant{}, err
+	}
+
+	result := make([]domain.Restaurant, len(restaurants))
+
+	for i, restaurant := range restaurants {
+		result[i] = *restaurant.ToDomain()
+	}
+
+	return result, nil
 }
 
 func (r *restaurantRepository) FindAllType() ([]domain.RestaurantType, error) {
-	return []domain.RestaurantType{}, nil
+	restaurantTypes := []models.RestaurantType{}
+
+	err := r.db.Find(&restaurantTypes).Error
+
+	if err != nil {
+		return []domain.RestaurantType{}, err
+	}
+
+	result := make([]domain.RestaurantType, len(restaurantTypes))
+
+	for i, restaurantType := range restaurantTypes {
+		result[i] = *restaurantType.ToDomain()
+	}
+
+	return result, nil
 }
 
 func (r *restaurantRepository) FindTypeById(id uint) (domain.RestaurantType, error) {
-	return domain.RestaurantType{}, nil
+	restaurantType := models.RestaurantType{}
+
+	err := r.db.Where("id = ?", id).First(&restaurantType).Error
+
+	return *restaurantType.ToDomain(), err
 }
